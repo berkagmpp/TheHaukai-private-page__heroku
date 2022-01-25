7e811f497cbda15b7148220cf3905f121b914d36
Claire <berkagmpp@gmail.com>
2022-01-25 19:44:34 +1300

    replace header and footer include all files

--- about_us.php
+++ about_us.php
@@ -15,75 +15,10 @@
 </head>
 
 <body>
-    <!-- Deleted <header> tag because of <nav> sticky-top classList -->
-    <!-- Top menu navigation -->
-    <nav class="navbar navbar-expand-lg bg-white">
-        <div class="container">
-            <!-- nav static space -->
-            <div id="navbar-static">
-                <a href="index.html" class="navbar-brant mb-0 h1">
-                    <img class="d-inline-block align-center"
-                        src="img/logo_109x38.png"
-                        width="109" height="38">
-                </a>
-                <button type="button" id="btn_menu"
-                    aria-label="Toggle navigation">
-                    <input type="image" class="--bs-white"
-                            src="img/bars.png" alt="Hamburger button">
-                </button>
-            </div>
-            <!-- nav list above lg size -->
-            <div class="navbar-dis" id="navbars-dis">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a href="index.html" class="nav-link active">HOME</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a href="about_us.html" class="nav-link active">ABOUT US</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a href="contact.html" class="nav-link active">CONTACT</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a href="blog.html" class="nav-link active">BLOG</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a href="menu.html" class="nav-link active">MENU</a>
-                    </li>
-                    <li class="nav-item active" id="booking-po">
-                        <a href="booking_buy.html" class="booking-box">BOOKING &amp; BUY</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- nav list up to md size -->
-            <div class="navbar-full bg-white" id="navbar-full">
-                <ul class="navbar-nav">
-                    <button type="button" id="btn_cancel">
-                        <input type="image" src="img/times.png" alt="Hamburger button">
-                    </button>
-                    <li class="nav-item active">
-                        <a href="index.html" class="nav-link active">HOME</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a href="about_us.html" class="nav-link active">ABOUT US</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a href="contact.html" class="nav-link active">CONTACT</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a href="blog.html" class="nav-link active">BLOG</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a href="menu.html" class="nav-link active">MENU</a>
-                    </li>
-                    <li class="nav-item active" id="booking-bo">
-                        <a href="booking_buy.html" class="booking-box">BOOKING &amp; BUY</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- End Header -->
+
+    <?php
+        include "header.html";
+    ?>
 
     <!-- Main -->
     <main class="row">
@@ -168,7 +103,9 @@
                                                     Recent Reviews:<br>
                                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                                                     sed do eiusmod tempor incididunt.</p>
-                        <li class="nav-item text-end"><a class="nav-link" href="#">Read More ></a></div>
+                        <div class="nav-item text-end">
+                            <a class="nav-link" href="#">Read More ></a>
+                        </div>
                     </div>
                 </article>
             </div>
@@ -197,55 +134,6 @@
     </main>
     <!-- End main -->
 
-    <!-- Footer -->
-    <footer>
-        <div class="container">
-            <div class="row py-2 py-sm-4">
-                <!-- footer links -->
-                <div class="col-lg-4 col-md-12 py-5 colFoot" aria-label="Footer links">
-                    <ul class="nav navbar-nav col-subnav">
-                        <li class="nav-item">
-                            <a class="nav-link p-3" href="privacy_policy.html">PRIVACY POLICY</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link p-3" href="#">TERMS &amp; CONDITIONS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link p-3" href="contact.html">CONTACT &amp; HELP</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link p-3" href="#">FOR EMPLOYEES</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- wheather info -->
-                <div class="col-lg-4 col-md-12 py-5 colFoot" aria-label="Wheather infomation">
-                    <h4 class="pb-4">WHEATHER INFO</h4>
-                    <div class="d-block mx-auto">
-                        <iframe id="widget-iframe" width="300px" height="167px" 
-                        src="https://services.metservice.com/weather-widget/widget?params=white|small|portrait|days-3|classic&amp;loc=auckland&amp;type=urban"></iframe>
-                    </div>
-                </div>
-                <!-- contact info -->
-                <div class="col-lg-4 col-md-12 py-5 mt-4 colFoot" aria-label="Contanct information">
-                    <span class="border border-2 rounded py-1 px-3 con-phone">09 401 4019</span>
-                    <p class="con-address">1 Island Street<br>Kerikeri, Bay of Islands<br>New Zealand</p>
-                </div>
-            </div>
-            <div class="container pb-5 colFoot" aria-label="Copyright display" aria-labelledby="copyright-text">
-                <span id="copyright-text" aria-label="Copyright information"></span>
-                <div class="font-09" aria-label="Copyright reserved">&nbsp;All Rights Reserved.</div>
-            </div>
-        </div>
-    </footer>
-    <!-- End footer -->
-
-    <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
-    <!-- custom javascript -->    
-    <script src="custom.js"></script>
-</body>
-
-</html>
+<?php
+    include "footer.php";
+?>
