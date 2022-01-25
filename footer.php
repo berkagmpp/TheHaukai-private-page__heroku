e8d9c042a81932e2ba29b04828bb5b316ca6d354
Claire <berkagmpp@gmail.com>
2022-01-26 08:14:37 +1300

    revised private.html

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
                             <a class="nav-link p-3" href="contact.php">CONTACT &amp; HELP</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link p-3" href="private.html">FOR EMPLOYEES</a>
+                            <a class="nav-link p-3" href="private/private.html">FOR EMPLOYEES</a>
                         </li>
                     </ul>
                 </div>
