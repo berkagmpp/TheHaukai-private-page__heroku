741e6eac23130186c0e89ac6004f36e86ad9c82a
Claire <berkagmpp@gmail.com>
2022-01-25 19:07:06 +1300

    replace header and footer html files

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 </head>
 
 <body>
+    
 <?php
     include "header.html";
 ?>
@@ -125,16 +126,6 @@
     </main>
     <!-- End main -->
 
-
 <?php
     include "footer.html";
 ?>
-    <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
-    <!-- custom javascript -->    
-    <script src="custom.js"></script>
-</body>
-
-</html>
