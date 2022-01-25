cafcf4584907f49a5a76306e303e449db85024a1
Claire <berkagmpp@gmail.com>
2022-01-26 12:43:07 +1300

    test index.html

--- index2.php
+++ index2.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="LP-Fd1PVUAonhWYGzvlRa7eYEj5616J2vzasuumK5Zg" />
+    <title lang="mi">HAUKAI RESTAURANT | Māori kai</title>
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- custom css-->
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+
+<body>
+
+    <?php
+        include "header.html";
+    ?>
+
     <!-- Main -->
     <main class="row">
         <!-- Index main banner -->
@@ -27,14 +49,14 @@
                             <div class="img-menu" role="img" aria-label="A lamb and salmon dish for menu page"></div>
                             <div id="text-menu" aria-labelledby="menus-page_link">
                                 <div class="img-cover">
-                                    <a href="menu.html"><h4 id="menus-page_link">OUR<br>MENUS</h4></a>
+                                    <a href="menu.php"><h4 id="menus-page_link">OUR<br>MENUS</h4></a>
                                 </div>
                             </div>
                         </div>
                         <div class="main-wrapper" aria-label="Main to about page link">
                             <div id="text-abouthaukai" aria-labelledby="about-page_link">
                                 <div class="img-cover">
-                                    <a href="about_us.html"><h4 id="about-page_link">ABOUT<br>HAUKAI</h4></a>
+                                    <a href="about_us.php"><h4 id="about-page_link">ABOUT<br>HAUKAI</h4></a>
                                 </div>
                             </div>
                             <div class="img-abouthaukai" role="img" aria-label="Table setting for about us page"></div>
@@ -47,7 +69,7 @@
                             <div class="img-location"></div>
                             <div id="text-location" aria-labelledby="location-page_link">
                                 <div class="img-cover" role="img" aria-label="Sunset for location page">
-                                    <a href="contact.html"><h4 id="location-page_link">LOCATION</h4></a>
+                                    <a href="contact.php#section-two"><h4 id="location-page_link">LOCATION</h4></a>
                                 </div>
                             </div>
                         </div>
@@ -65,42 +87,61 @@
                         <div class="img-fluid">
                             <img src="img/blog_1_400x400.jpg" alt="The potatoes pouring from the envelope in the black background">
                         </div>
-                        <h4 class="mt-3">Potato dishes from the central North Island</h4>
-                        <p class="text-end">2021.12.30</p>
-                        <p class="pr-5 p-clamp">
-                            Lorem ipsum dolor sit amet, consec adiping elit,
-                            sed do eiuso tempor incdunt ut labre et dolore magna alqua.
-                            Semper feugia nibh sed pulvinar proin gravida. Vel orci porta non pulvinar neque laoreet suspendisse.
-                        </p>
+                        <div class="p-3 text-cover">
+                            <h4 class="mt-3">Potato dishes from the central North Island</h4>
+                            <p class="text-end">2021.12.30</p>
+                            <p class="pr-5 p-clamp">
+                                Lorem ipsum dolor sit amet, consec adiping elit,
+                                sed do eiuso tempor incdunt ut labre et dolore magna alqua.
+                                Semper feugia nibh sed pulvinar proin gravida. Vel orci porta non pulvinar neque laoreet suspendisse.
+                            </p>
+                            <div class="nav-item text-end">
+                                <a class="nav-link-dark p-0" href="blog.php#section-one">Read More ></a>
+                            </div>
+                        </div>
                     </div>
                     <!-- main post 2 -->
                     <div class="col-lg-4 col-md-12 px-3 my-5 colPost" aria-label="Blog post two">
                         <div class="img-fluid">
                             <img src="img/blog_2_400x400.jpg" alt="Six Oysters and two sliced lemons in a round blue plate">
                         </div>
-                        <h4 class="mt-3">Fusion style using Bluff Oyster</h4>
-                        <p class="text-end">2022.01.15</p>
-                        <p class="pr-5 p-clamp">
-                            Lorem ipsum dolor sit amet, consec adiping elit,
-                            sed do eiuso tempor incdunt ut labre et dolore magna alqua.
-                            Semper feugia nibh sed pulvinar proin gravida. Vel orci porta non pulvinar neque laoreet suspendisse.
-                        </p>
+                        <div class="p-3 text-cover">
+                            <h4 class="mt-3">Fusion style using Bluff Oyster</h4>
+                            <p class="text-end">2022.01.15</p>
+                            <p class="pr-5 p-clamp">
+                                Lorem ipsum dolor sit amet, consec adiping elit,
+                                sed do eiuso tempor incdunt ut labre et dolore magna alqua.
+                                Semper feugia nibh sed pulvinar proin gravida. Vel orci porta non pulvinar neque laoreet suspendisse.
+                            </p>
+                            <div class="nav-item text-end">
+                                <a class="nav-link-dark p-0" href="blog.php#section-one">Read More ></a>
+                            </div>
+                        </div>
                     </div>
                     <!-- main post 3 -->
                     <div class="col-lg-4 col-md-12 px-3 my-5 colPost" aria-label="Blog post three">
                         <div class="img-fluid">
                             <img src="img/blog_3_400x400.jpg" alt="Round stones that are being heated up by fire">
                         </div>
-                        <h4 class="mt-3">The ingredients of Hāngi using heated rocks</h4>
-                        <p class="text-end">2022.02.06</p>
-                        <p class="pr-5 p-clamp">
-                            Lorem ipsum dolor sit amet, consec adiping elit,
-                            sed do eiuso tempor incdunt ut labre et dolore magna alqua.
-                            Semper feugia nibh sed pulvinar proin gravida. Vel orci porta non pulvinar neque laoreet suspendisse.
-                        </p>
+                        <div class="p-3 text-cover">
+                            <h4 class="mt-3">The ingredients of Hāngi using heated rocks</h4>
+                            <p class="text-end">2022.02.06</p>
+                            <p class="pr-5 p-clamp">
+                                Lorem ipsum dolor sit amet, consec adiping elit,
+                                sed do eiuso tempor incdunt ut labre et dolore magna alqua.
+                                Semper feugia nibh sed pulvinar proin gravida. Vel orci porta non pulvinar neque laoreet suspendisse.
+                            </p>
+                            <div class="nav-item text-end">
+                                <a class="nav-link-dark p-0" href="blog.php#section-one">Read More ></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
     </main>
     <!-- End main -->
+
+<?php
+    include "footer.html";
+?>
